594c44e3abaaffe5c4af66695e3188d3424631ad
Кардаков Денис Алексеевич <Denis.Kardakov@norbit.ru>
2022-01-28 01:48:54 +0300

    add code to future deals

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ import {router} from "./routes/aircraft.routes.js"
 const PORT = process.env.port || 5000;
 const app = express();
 
+app.route('/book')
+    .get(function (req, res) {
+        res.send('Get a random book')
+    })
+    .post(function (req, res) {
+        res.send('Add a book')
+    })
+    .put(function (req, res) {
+        res.send('Update the book')
+    })
+
 app.use('/api',router)
 
 app.listen(PORT, () => console.log("server started on port " + PORT));
--- routes/aircraft.routes.js
+++ routes/aircraft.routes.js
@@ -3,8 +3,6 @@ import {AircraftController} from '../controller/aircraft.controller.js'
 const {api} = AircraftController();
 
 
-const router = new express()
+export const router = new express()
 router.get('/aircrafts', (req,res)=>api.getAircrafts(req,res))
 
-
-export {router}
